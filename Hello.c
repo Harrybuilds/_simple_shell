cbfbc1e21777b7f53276148d8b1d7c668578c04d
Ana-sta-44 <ugwuokechisom123@gmail.com>
2023-05-15 05:57:59 -0700

    Hello.c

---
+++ Hello.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * description: Write a program that prints Hello.c
+ *
+ * Return: Nothing
+ */
+int main(void)
+{
+	printf("Hello.c\n");
+	return (0);
+}
